86a6c955c93692b5c9bf73a7e8c3abbc70aecc8e
Big_P <wdpeng24@gmail.com>
2016-06-22 10:17:36 +0800

    new project

---
+++ testSizeClass/testSizeClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testSizeClass
+//
+//  Created by dapeng on 16/6/22.
+//  Copyright © 2016年 kascend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
